64824c1baff4e85de5d0016c5c251a7a3d88cab8
Daehyeon Bae <noeyheadb@gmail.com>
2021-02-02 15:30:32 +0900

    🎨 Add util functions into /__init__.py

--- chipwhisperer_lite_wrapper/__init__.py
+++ chipwhisperer_lite_wrapper/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['CWLiteWrapper', 'CWTrace']
+__all__ = ['CWLiteWrapper', 'CWTrace', 'visualization_single_trace', 'cw_lite_firmware_update_automatically']
 
 from .cw_lite_wrapper import CWLiteWrapper
 from .cw_trace import CWTrace
+from .utils import visualization_single_trace, cw_lite_firmware_update_automatically
